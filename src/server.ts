40739c0b5981c7a2a49d73ee88440187d999b4bb
Gabriela Moura <gabrieladaniel49@hotmail.com>
2026-04-03 11:53:55 -0300

    feature: adicionar o middleware de erro global no server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import router from "./interfaces/http/routes";
+import { errorHandler } from "./interfaces/http/middlewares/errorHandler";
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("🚀 Server running on port 3000");
